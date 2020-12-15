575118976fd2a129bf767ca888506c9ac7e95bd9
canacar9407 <canacar94@gmail.com>
2020-12-15 15:40:53 +0000

    ödevhafta3_çözümü

--- script.js
+++ script.js
@@ -1,53 +1,66 @@
-function Animal(name){
-    this.name = name;
+class Animal {
+    constructor(name){
+        this.name=name;
+    }
+    action(){
+        document.getElementById(this.actionSoundName).play();
+    }
+    image(){
+        document.getElementById(this.resim).style.visibility='visible';
+    }
+    putInTheDocument(){
+        var petsTable = document.getElementById("petsTable");
+        var petTR = document.createElement("tr");
+
+        var petNameTD = document.createElement("td");
+        petNameTD.textContent = this.name;
+        petTR.appendChild(petNameTD);
+        petNameTD.onclick = this.image.bind(this);
+        petNameTD.addEventListener("mouseover", function(e){ 
+          petNameTD.bgColor="green",petNameTD.style.color ="white";});
+        petNameTD.addEventListener("mouseout", function(e){ 
+          petNameTD.bgColor="transparent",petNameTD.style.color ="black";});
+       
+
+        var petLegsTD = document.createElement("td");
+        petLegsTD.textContent = this.legs;
+        petTR.appendChild(petLegsTD);
+        petLegsTD.onclick = this.image.bind(this);
+        petLegsTD.addEventListener("mouseover", function(e){ 
+          petLegsTD.bgColor="green", petLegsTD.style.color ="white"; });
+        petLegsTD.addEventListener("mouseout", function(e){ 
+          petLegsTD.bgColor="transparent", petLegsTD.style.color ="black"; });
+
+        var petActionTD = document.createElement("td");
+        var petActionTDButton = document.createElement("button");
+        petActionTDButton.textContent = this.actionText;
+        petActionTD.appendChild(petActionTDButton);
+        petTR.appendChild(petActionTD);
+
+        petActionTDButton.onclick = this.action.bind(this);
+        petsTable.querySelector("tbody").appendChild(petTR)
+    }
 }
-
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
-  
+class Cat extends Animal{
+    constructor(name){
+        super(name);
+        //Animal.call(this, name);
+        this.legs = 4;
+        this.actionText = "Meoow"
+        this.actionSoundName = "meow"
+        this.resim="kedi resmi"
+    }
 }
-
-Animal.prototype.putInTheDocument = function(){
-    var petsTable = document.getElementById("petsTable");
-    var petTR = document.createElement("tr");
-  
-    var petNameTD = document.createElement("td");
-    petNameTD.textContent = this.name;
-    petTR.appendChild(petNameTD);
-  
-    var petLegsTD = document.createElement("td");
-    petLegsTD.textContent = this.legs;
-    petTR.appendChild(petLegsTD);
-  
-    var petActionTD = document.createElement("td");
-    var petActionTDButton = document.createElement("button");
-    petActionTDButton.textContent = this.actionText;
-    petActionTD.appendChild(petActionTDButton);
-    petTR.appendChild(petActionTD);
-    
-    petActionTDButton.onclick = this.action.bind(this);
-    petsTable.querySelector("tbody").appendChild(petTR)
+class Monkey extends Animal{
+    constructor(name){
+        super(name);
+        //Animal.call(this, name);
+        this.legs = 2;
+        this.actionText = "Scream";
+        this.actionSoundName = "scream";
+        this.resim="maymun resmi"
+    }
 }
-
-
-function Cat(name){
-    Animal.call(this, name);
-    this.legs = 4;
-    this.actionText = "Meoow"
-    this.actionSoundName = "meow"
-}
-
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
-    this.legs = 2;
-    this.actionText = "Scream";
-    this.actionSoundName = "scream";
-}
-
-Monkey.prototype = Animal.prototype;
-
 var Mila = new Cat("Mila");
 Mila.putInTheDocument();
 
